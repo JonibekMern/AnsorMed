8e828131a7b46a95ddd9fb5f0b1253a90f8e41a3
Jonibek <logistulugbekovich@gmail.com>
2023-04-14 07:37:20 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// header classlist add open
+const header = document.querySelector("header"),
+  burgerMenu = document.querySelector(".burger-menu");
+
+burgerMenu.addEventListener("click", () => {
+  header.classList.toggle("open");
+});
+// header classlist add open end
